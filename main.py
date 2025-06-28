c541432a69b8e84cdf6b41a630d5cea26354a17c
Liam <l0i4a1m5@gmail.com>
2025-06-28 16:40:36 +0900

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess, sys
-
+import ast
 
 def installpkg(packagename):
     subprocess.check_call([sys.executable, "-m", "pip", "install", packagename])
@@ -9,16 +9,7 @@ try:
 except ImportError as IE:
     installpkg("pyinstaller")
 
-
-class TypedValue:
-    def __init__(self, value, expected_type):
-        self.value = value
-        self.expected_type = expected_type
-    
-    def checkTE(self):
-        if not isinstance(self.value, self.expected_type):
-            raise TypeError(f"Expected {self.expected_type} as type for value {self.value}")
-    
+   
 
 file_name = input("pypp>>>")
 #input style is like pypp_filename -o py_filename
@@ -33,22 +24,38 @@ with open(file_name[0].strip(), "r") as f:
 #main cause im too lazy
 with open(file_name[1].strip(), "w") as destfile:
     print("Convert Init\n\n\n\n\n\n\n\n")
+    exetarfile = tarfile
     tarfile = tarfile.splitlines()
-    declared_types = {}
+
+    #class for variables and types
+    typedvalue_code = [
+        "class TypedValue:",
+        "    def __init__(self, value, expected_type):",
+        "        self.value = value",
+        "        self.expected_type = expected_type",
+        "",
+        "    def checkTE(self):",
+        "",
+        "",
+        "        if not isinstance(self.value, self.expected_type):",
+        "            if self.expected_type == 'var':",
+        "                 pass",
+        "            else:",
+        "                 print('ERROR: Wrong DataType error!')",
+        "                 exit()",
+        "",
+        ""
+    ]
+
+    for line in typedvalue_code:
+        destfile.write(line + "\n")
+
     #doing line by line
-    destfile.write("""
-    class TypedValue:
-        def __init__(self, value, expected_type):
-            self.value = value
-            self.expected_type = expected_type
-
-        def checkTE(self):
-            if not isinstance(self.value, self.expected_type):
-                raise TypeError(f"Expected {self.expected_type.__name__} as type for value {self.value}")
-    """.strip() + "\n\n")
-
     for line in tarfile:
-        if line:
+        if line:            
+            if line[-1] == ";":
+                line = line.strip(";")
+
             if line[0] == "*":
                 line = line[1:].strip()
                 parts = line.split("=", 1)
@@ -57,15 +64,16 @@ with open(file_name[1].strip(), "w") as destfile:
                 right = parts[1].strip()
 
                 typename, varname = left.split()
+                if typename == "var":
+                    destfile.write(f"{varname} = TypedValue({right}, '{typename}')\n")
 
-                declared_types[varname] = typename
+                else:
+                    destfile.write(f"{varname} = TypedValue({right}, {typename})\n")
+                    destfile.write(f"{varname}.checkTE()\n")
 
-                destfile.write(f"{varname} = TypedValue({right}, {typename})\n")
-                destfile.write(f"{varname}.checkTE()\n")
             else:
-                destfile.write(line)
+                destfile.write(f"{line}\n")
+
         
         else:
             continue
-
-        print(line)
--- tar.py
+++ tar.py
@@ -1,12 +1,26 @@
 class TypedValue:
-        def __init__(self, value, expected_type):
-            self.value = value
-            self.expected_type = expected_type
+    def __init__(self, value, expected_type):
+        self.value = value
+        self.expected_type = expected_type
 
-        def checkTE(self):
-            if not isinstance(self.value, self.expected_type):
-                raise TypeError(f"Expected {self.expected_type.__name__} as type for value {self.value}")
+    def checkTE(self):
+
+
+        if not isinstance(self.value, self.expected_type):
+            if self.expected_type == 'var':
+                 pass
+            else:
+                 print('ERROR: Wrong DataType error!')
+                 exit()
+
 
 x = TypedValue(12345, int)
 x.checkTE()
 print(x.value)
+name = TypedValue(input("Hello whats your name?"), str)
+name.checkTE()
+print(f"Hello{name.expected_type}!")
+name = TypedValue("hi", str)
+name.checkTE()
+print(name.value)
+input("")
